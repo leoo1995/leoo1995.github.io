94ff33741cfa19bb85d2acd64d7f880ebb4923c4
Luis Ortiz <leoo1995@hotmail.com>
2021-08-29 11:06:49 -0500

    skills section margin fixed

--- src/components/Skills/styles.js
+++ src/components/Skills/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 export const Wrapper = styled.div`
   width: calc(100% - 40px);
-  margin: 40pxs auto;
+  margin: 40px auto;
   display: flex;
   flex-direction: column;
   align-items: center;
